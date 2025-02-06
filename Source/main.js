a4b4c26b87cb973a9e492a9e91bd4c4585c1119d
Nurbek <rayimberganovnurbk6@gmai.com>
2025-02-06 15:47:40 +0500

    5 fevral

--- Source/main.js
+++ Source/main.js
@@ -1,31 +1,14 @@
+console.log('ha')
 let bi_list = document.querySelector('.bi-list')
 let bi_x = document.querySelector('.bi-x')
 let header = document.getElementById('header')
 bi_list.addEventListener('click' , ()=>{
-    // header.classList.remove('d-none')
-    // header.classList.add('d-block')
-
-    // bi_list.classList.remove('d-block')
-    // bi_list.classList.add('d-none')
-
-    // bi_x.classList.add('d-block')
-    // bi_x.classList.remove('d-none')
-
-    bi_x.classList.add('d-none')
+    bi_list.classList.add('d-none')
     bi_x.classList.remove('d-none')
     header.style.marginLeft = '0px'
 })
 bi_x.addEventListener('click' , ()=>{
-    // header.classList.remove('d-block')
-    // header.classList.add('d-none')
-
-    // bi_list.classList.remove('d-none')
-    // bi_list.classList.add('d-block')
-
-    // bi_x.classList.add('d-none')
-    // bi_x.classList.remove('d-block')
-
     bi_x.classList.add('d-none')
-    bi_x.classList.remove('d-none')
+    bi_list.classList.remove('d-none')
     header.style.marginLeft = '-300px'
 })
